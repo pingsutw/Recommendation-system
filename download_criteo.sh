50fb4b6c6124458e240e29a0a63ab56f3fc902d6
pingsutw <pingsutw@gmail.com>
2020-05-16 13:53:23 +0800

    Add preprocessing data

---
+++ download_criteo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://labs.criteo.com/2014/02/download-kaggle-display-advertising-challenge-dataset/
+tar -zxvf dac.tar.gz
---
+++ run_deepfm.py
@@ -0,0 +1,18 @@
+from submarine.ml.model import DeepFM
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-conf", help="a JSON configuration file for FM", type=str)
+    parser.add_argument("-task_type", default='train',
+                        help="train or evaluate, by default is train")
+    args = parser.parse_args()
+    json_path = args.conf
+    task_type = args.task_type
+
+    model = DeepFM(json_path=json_path)
+
+    if task_type == 'train':
+        model.train()
+        result = model.evaluate()
+        print("Model metrics : ", result)
